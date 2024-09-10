abedf4bfee80218ce00c6c5779672725d73be5af
Pui <nattiyakrajangpit@gmail.com>
2024-09-10 18:15:17 -0400

    add cont

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
     const handleAddToCart = (product) => {
+        if (addedToCart[product.id]) return;
         dispatch(addItem(product));
         setAddedToCart((prevState) => ({
             ...prevState,
